a87d7cc14644e9ca6d9c05a04c06e1a9ca7fbb00
yessmin_bouchehed <154808595+yessminbd@users.noreply.github.com>
2025-08-07 21:12:59 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,6 @@
 import projet1 from "../assets/projet1.png"
 import projet3 from "../assets/projet3.png"
+import projet2 from "../assets/projet2.png"
 export const HERO_CONTENT = "Développeuse web junior spécialisée dans la stack MERN, je suis passionnée par la création de solutions web modernes et efficaces. En consolidant mes compétences en développement front-end et back-end, j’aborde chaque projet avec curiosité, rigueur et le souci d’un code propre. Je m’investis pleinement dans l’apprentissage des technologies actuelles afin de proposer des applications performantes et adaptées aux besoins réels.";
 
 export const ABOUT_CONTENT = "Titulaire d’une licence en informatique appliquée à la gestion, je me spécialise dans le développement web full-stack avec la stack MERN. Lors de mon Projet de Fin d’Études, j’ai travaillé pour la première fois avec cette stack, qui m’a particulièrement attirée par sa polyvalence et son efficacité. Mon parcours est guidé par l’autoformation, l’exploration de technologies variées et une forte capacité à résoudre des problèmes. Je suis engagée dans une démarche de progression continue, avec pour objectif de concevoir des interfaces web fonctionnelles, esthétiques et impactantes.";
@@ -34,7 +35,16 @@ export const PROJECTS = [
             "en assurant une expérience utilisateur optimisée sur tous les supports (ordinateurs, tablettes, mobiles). " +
             "La partie backend est présente mais allégée, le focus principal restant sur la qualité et la réactivité du front-end.",
         technologies: ["React", "Node.js", "MongoDB", "Express.js", "TailwindCSS"]
-    }
+    },
+    {
+    url: "https://mealora.vercel.app/",
+    title: "Mealora – Site vitrine de cuisine tunisienne",
+    image: projet2,
+    description:
+        "Mealora est un site vitrine moderne dédié à la cuisine tunisienne, mettant en valeur des plats traditionnels à travers une interface élégante et responsive. Le site propose une expérience utilisateur immersive, avec une navigation fluide, des images alléchantes, et une mise en page soignée. Ce projet met l’accent sur le design et la présentation des spécialités culinaires dans un style contemporain, respectant l'identité culturelle. Il est déployé sur Vercel et optimisé pour le SEO.",
+    technologies: ["React", "Vite", "TailwindCSS", "JavaScript", "Vercel"]
+}
+
 ]
 
 
@@ -42,4 +52,5 @@ export const CONTACT = {
     address: "8024 Nabeul Tunisia",
     PhoneNo: "+216 54 121 578",
     email: "yessminbouchehed@gmail.com"
+
 }
